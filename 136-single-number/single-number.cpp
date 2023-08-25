c26c8c7ffc57131841ad2facf898ef688a4954a0
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2023-08-25 15:21:06 +0530

    Time: 16 ms (70.21%) | Memory: 16.9 MB (67.29%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -6,23 +6,20 @@ public:
     int s=nums.size()-1;
     for(i=0;i<s;i++)
     {
-       if(i==0)
+       if(i>0&&i<s-1)
        {
-           cout<<1;
-           if (nums[i+1]!=nums[i])
-                return nums[i];
-       }
-       else if(i>0&&i<s-1)
-       {
-           cout<<2;
             if(nums[i-1]!=nums[i]&&nums[i]!=nums[i+1])
-                return nums[i];
+                break;
+       }
+       else if(i==0)
+       {
+            if (nums[i+1]!=nums[i])
+                break;
        }
        else
        {
-           cout<<3;
-       if(nums[i+1]!=nums[i])
-       return nums[i+1]; 
+            if(nums[i+1]!=nums[i])
+                return nums[i+1]; 
        }      
     }
     return nums[i];
