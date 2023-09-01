f01fb7a23c5f84c7f0159108729a5689d633c5f9
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2023-09-01 15:11:36 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (11.97%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-    int len=s.length(),i,f=0;
+    int len=s.length(),i,f=0,c=0;
     for(i=len-1;i>=0;--i)
     {
         if(s[i]==' '&&f==1)
         break;
         else if(s[i]!=' '&&f==0)
         f=1;
+        else if(s[i]==' '&&f==0)
+        ++c;
     }
-    f=0;
-    ++i;
-    for(;s[i]!=NULL;++i)
-    {
-        if(s[i]==' ')
-        break;
-        else
-        ++f;
-    }
-    return f;
+    return len-(i+1)-c;
     }
 };
