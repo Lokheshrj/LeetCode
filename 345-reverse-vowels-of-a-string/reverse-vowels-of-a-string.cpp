cb36f2490bf78b9739c987fcd3af9939abdace41
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2023-09-24 20:32:52 +0530

    Time: 7 ms (57.36%) | Memory: 8.5 MB (12.02%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -16,23 +16,5 @@ public:
                 ++c;
             }
             return s;
-        /*char temp;
-        int j=s.length()-1;
-        for(int i=0;i<s.length()/2;i++)
-        {
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
-            {
-                
-                while((!(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u'||s[j]=='A'||s[j]=='E'||s[j]=='I'||s[j]=='O'||s[j]=='U'))&&(j>s.length()/2))
-                --j;
-                if((s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')&&(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u'||s[j]=='A'||s[j]=='E'||s[j]=='I'||s[j]=='O'||s[j]=='U'))
-                {
-                    temp=s[i];
-                    s[i]=s[j];
-                    s[j]=temp;
-                    --j;
-                }
-            }
-        }*/
     }
 };
