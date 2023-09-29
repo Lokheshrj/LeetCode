752845b2505cd421713ad27f356267b28b23d7b4
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2023-09-29 23:25:21 +0530

    Time: 48 ms (13.07%) | Memory: 19.5 MB (28.20%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int zc=0,p=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+                ++zc;
+            else
+            {
+                nums[p]=nums[i];
+                ++p;
+            }
+        }
+        while(zc--)
+        {
+            nums[p]=0;
+            ++p;
+        }
+        for(int x:nums)
+        cout<<x;
+    }
+};
