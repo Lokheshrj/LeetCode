2ed2cbe244daac17aeb4b8a270ffc922002b1ea5
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2023-10-14 23:02:25 +0530

    Time: 104 ms (29.12%) | Memory: 112.1 MB (28.31%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -4,13 +4,13 @@ public:
         int l=INT_MAX;
         int m=INT_MAX;
         for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]<l)
-            l=nums[i];
+        { 
+            if(nums[i]>m)
+            return true;
             else if(nums[i]>l&&nums[i]<m)
             m=nums[i];
-            else if(nums[i]>m)
-            return true;
+            else if(nums[i]<l)
+            l=nums[i];
         }
         return false;
     }
