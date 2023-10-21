6b58924610ad64234566bb3df77e77f75e07e11c
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2023-10-21 23:29:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (6.50%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -3,22 +3,19 @@ public:
     void count(vector<char>&s,int c)
     {       
         if(c<10)
-        {
-        s.push_back(c+48);
-        return;
-        }
-        int l;
-        if(c<100)
-        l=10;
-        else if(c<1000)
-        l=100;
+            s.push_back(c+48);
         else
-        l=1000;
-        while(l>0)
         {
-            s.push_back((c/l)+48);
-            c%=l;
-            l/=10;
+            int l;
+            if(c<100) l=10;
+            else if(c<1000) l=100;
+            else l=1000;
+            while(l>0)
+            {
+                s.push_back((c/l)+48);
+                c%=l;
+                l/=10;
+            }
         }
         return;
     }
