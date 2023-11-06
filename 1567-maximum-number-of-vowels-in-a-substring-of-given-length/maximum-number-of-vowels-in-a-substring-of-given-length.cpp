e94c56c4d5e1113163aa8af7c019d28b3ba0bc5f
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2023-11-06 15:31:19 +0530

    Time: 19 ms (76.62%) | Memory: 10.2 MB (83.78%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -3,23 +3,21 @@ public:
     int maxVowels(string s, int k) 
     {
         int i;
-        int max=0;
-        int c=0;
+        int m,c=0;
         for(i=0;i<k;i++)
             if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
                 ++c;
-        max=c;
-        cout<<max;
+        m=c;
         while(i<s.length())
         {
             if(s[i-k]=='a'||s[i-k]=='e'||s[i-k]=='i'||s[i-k]=='o'||s[i-k]=='u')
             --c;
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') ++c;
-            cout<<c;
-            if(c>max)
-            max=c;
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') 
+            ++c;
+            if(c>m)
+            m=c;
             ++i;
         }
-        return max;
+        return m;
     }
 };
