6b8faf55e41a6218e68b203eeb61ed955f867d5a
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2023-11-07 21:39:42 +0530

    Time: 94 ms (37.27%) | Memory: 26.2 MB (58.17%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -3,12 +3,9 @@ public:
     string removeStars(string s) 
     {
         string t;
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<s.length();i++)
+        {
              (s[i]=='*')?t.pop_back(): t.push_back(s[i]);
-            /*if(s[i]=='*')
-            t.pop_back();
-            else 
-            t.push_back(s[i]);*/
         }
         return t;   
     }
