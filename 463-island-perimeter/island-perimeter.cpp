af78baf51c2729703ba41f920287651d630cff19
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-01-09 15:17:56 +0530

    Time: 77 ms (74.68%) | Memory: 96.6 MB (55.02%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -12,10 +12,6 @@ public:
                 {
                     p+=4;
                     grid[i][j]=-1;
-                    if(i+1<r&&grid[i+1][j]==-1)
-                    p-=2;
-                    if(j+1<c&&grid[i][j+1]==-1)
-                    p-=2;
                     if(i>0&&grid[i-1][j]==-1)
                     p-=2;
                     if(j>0&&grid[i][j-1]==-1)
