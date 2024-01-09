1275eb8022bbd299832c427b557f632eae9fb9a6
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-01-09 19:36:23 +0530

    Time: 6 ms (18.01%) | Memory: 8.9 MB (9.37%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -4,9 +4,8 @@ public:
     {
         map<int,int>occur;
         set<int>unique;
-        int n=arr.size();
-        for(int i=0;i<n;i++)
-            occur.insert({arr[i],count(arr.begin(),arr.end(),arr[i])});
+        for(auto x:arr)
+        occur[x]++;
         for(auto it=occur.begin();it!=occur.end();++it)
             unique.insert(it->second);
         return occur.size()==unique.size();
