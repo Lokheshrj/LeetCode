79fcf431894eecf1936d12ed70d9b7a1b2da0fb7
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-02-20 16:20:39 +0530

    Time: 7 ms (55.18%) | Memory: 23.7 MB (30.07%) - LeetSync

--- 3221-find-the-peaks/find-the-peaks.cpp
+++ 3221-find-the-peaks/find-the-peaks.cpp
@@ -4,14 +4,10 @@ public:
     {
         vector<int>res;
         int n=mountain.size()-1;
-        int l=0;
-        int h=2;
         for(int i=1;i<n;i++)
         {
-            if(mountain[i]>mountain[l]&&mountain[i]>mountain[h])
+            if(mountain[i]>mountain[i-1]&&mountain[i]>mountain[i+1])
             res.push_back(i);
-            ++l;
-            ++h;
         }
         return res;
     }
