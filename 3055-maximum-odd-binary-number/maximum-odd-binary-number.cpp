0f616357f8cd2e171ccda92c1bfdf2c32c5a0aa0
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-03-05 18:37:50 +0530

    Time: 9 ms (7.93%) | Memory: 9.6 MB (22.38%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -8,24 +8,14 @@ public:
         int n=s.length()-1;
         for(int i=0;i<=n;i++)
         s[i]='0';
-        if(m['1']==1)
-        {
-            s[n]='1';
-            return s;
-        }
-        else if (m['1']==2)
-        {
+        if (m['1']==2)
             s[0]='1';
-            s[n]='1';
-            return s;
-        }
-        else
+        else if(m['1']>2)
         {
             for(int i=0;i<m['1']-1;i++)
             s[i]='1';
-            s[n]='1';
-            return s;
         }
-        return "";
+        s[n]='1';
+        return s;
     }
 };
