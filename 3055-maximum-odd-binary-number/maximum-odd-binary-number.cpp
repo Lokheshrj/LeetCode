91c603eb7a88dc13f58bed3121be815ac0a6cc8c
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-03-05 18:41:05 +0530

    Time: 7 ms (24.17%) | Memory: 8.1 MB (99.23%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -2,17 +2,21 @@ class Solution {
 public:
     string maximumOddBinaryNumber(string s) 
     {
-        map<char,int>m;
-        for(char x:s)
-        m[x]++;
+        short int c=0;
         int n=s.length()-1;
         for(int i=0;i<=n;i++)
-        s[i]='0';
-        if (m['1']==2)
-            s[0]='1';
-        else if(m['1']>2)
         {
-            for(int i=0;i<m['1']-1;i++)
+            if(s[i]=='1')
+            {
+                ++c;
+                s[i]='0';
+            }
+        }
+        if (c==2)
+            s[0]='1';
+        else if(c>2)
+        {
+            for(int i=0;i<c-1;i++)
             s[i]='1';
         }
         s[n]='1';
