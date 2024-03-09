8b88474ff5a22fea7821c01c7ebb9afef11cb3a0
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-03-09 19:40:04 +0530

    Time: 66 ms (76.91%) | Memory: 52.8 MB (73.96%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -3,8 +3,8 @@ public:
     int getCommon(vector<int>& nums1, vector<int>& nums2)
     {
         int i=0,j=0;
-        int n1=nums1.size(),n2=nums2.size();
-        while(i<n1&&j<n2)
+        //int n1=nums1.size(),n2=nums2.size();
+        while(i<nums1.size()&&j<nums2.size())
         {
             if(nums1[i]==nums2[j])
             return nums1[i];
