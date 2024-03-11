0bd6917b0fe0e8daf327373266360c0136119c9b
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-03-11 12:06:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (66.00%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -5,20 +5,20 @@ public:
         vector<int>mp(26,0);
         for(char ch:s)
         ++mp[ch-'a'];
-        string samp="";
+        s="";
         for(char ch:order)
         {
             if(mp[ch-'a'])
             {
-                samp+=string(mp[ch-'a'],ch);
+                s+=string(mp[ch-'a'],ch);
                 mp[ch-'a']=0;
             }
         }
         for(int i=0;i<26;i++)
         {
             if(mp[i])
-            samp+=string(mp[i],i+'a');
+            s+=string(mp[i],i+'a');
         }
-        return samp;
+        return s;
     }
 };
