2cadbc54792fbfcf9d57473dbb66d48d31bbc9db
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-07-23 14:50:42 +0530

    Time: 10 ms (29.58%) | Memory: 15.2 MB (14.29%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -2,14 +2,7 @@ class Solution {
 public:
     static bool comparator(pair<int,int>&A,pair<int,int>&B)
     {
-        if(A.second<B.second)
-            return true;
-        else if(A.second==B.second)
-        {
-            if(A.first>B.first)     
-            return true;
-        }
-        return false;
+       return  (A.second<B.second || (A.second==B.second && A.first>B.first))?true:false;
     }
     vector<int> frequencySort(vector<int>& nums) {
        unordered_map<int,int>freq;
@@ -20,8 +13,6 @@ public:
             arr.push_back(it);
         sort(arr.begin(),arr.end(),comparator);
         int c=0;
-        for(auto x:arr)
-            cout<<x.first<<" "<<x.second<<"\n";
         for(auto x:arr)
         {
             fill(nums.begin()+c,nums.begin()+x.second+c,x.first);
