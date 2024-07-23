2dda435ef911b867aae0e92b2f44ad602cb2d38e
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-07-23 13:27:53 +0530

    Time: 18 ms (7.67%) | Memory: 15.2 MB (14.29%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    static bool comparator(pair<int,int>&A,pair<int,int>&B)
+    {
+        if(A.second<B.second)
+            return true;
+        else if(A.second==B.second)
+        {
+            if(A.first>B.first)     return true;
+        }
+        return false;
+    }
+    vector<int> frequencySort(vector<int>& nums) {
+       unordered_map<int,int>freq;
+       for(auto x:nums)
+            ++freq[x];
+        vector<pair<int,int>>arr;
+        for(auto &it:freq)
+            arr.push_back(it);
+        sort(arr.begin(),arr.end(),comparator);
+        
+        for(auto x:arr)
+            cout<<x.first<<" "<<x.second<<"\n";
+        int c=0;
+        for(auto x:arr)
+        {
+            for(int i=0;i<x.second;i++)
+            {
+                nums[c]=x.first;
+                ++c;
+            }
+                
+        }
+        return nums;
+    }
+};
