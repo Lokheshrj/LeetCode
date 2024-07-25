ba913975b3ac7a8cafc42563e233c07cf29a4ce6
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-07-25 13:01:43 +0530

    Time: 850 ms (10.76%) | Memory: 389.9 MB (6.38%) - LeetSync

---
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int converter(vector<int>&m,int n)
+    {
+        stack<int>temp;
+        int mv=0;
+        if(n==0)
+            temp.push(n);
+        while(n>0)
+        {
+            temp.push(n%10);
+            n/=10;
+        }
+        while(!temp.empty())
+        {
+            mv*=10;
+            mv+=m[temp.top()];
+            temp.pop();
+        }
+        return mv;
+    }
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) 
+    {
+        int t;
+        map<int,list<int>>maps;
+        short int s=nums.size();
+        cout<<"result :\n";
+        for(int i=0;i<s;i++)
+        {
+            t=converter(mapping,nums[i]);
+            maps[t].push_back(nums[i]);
+            cout<<t;
+            nums[i]=t;  
+        }
+        sort(nums.begin(),nums.end());
+       /* cout<<"Result \n:";
+        for(auto n:nums)
+            cout<<n<<" ";*/
+        vector<int>ans;
+        for(auto i:nums)
+        {
+            ans.push_back(maps[i].front());
+            maps[i].pop_front();
+        }
+        return ans;
+    }
+};
