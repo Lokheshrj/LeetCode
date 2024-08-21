342db5a8f99798ee238f17ca76e08a883c86daad
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-08-21 11:55:56 +0530

    Time: 2 ms (44.14%) | Memory: 10.5 MB (33.63%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -5,14 +5,12 @@ public:
         int i=digits.size()-1;
         for(;i>=0;--i)
         {
-            if(digits[i]>=0&&digits[i]<=8)
+            if(digits[i]==9)
+                digits[i]=0;
+            else
             {
                 digits[i]+=1;
                 break;
-            }
-            else 
-            {
-                digits[i]=0;
             }
         }
         if(i==-1)
