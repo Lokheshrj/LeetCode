22736cbc413d95ca8e63f4e80bd392592eeb32fd
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-09-13 18:40:08 +0530

    Time: 82 ms (20.99%) | Memory: 44.8 MB (20.02%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
+    void Prefix_Xor(vector<int>&a,vector<int>&arr,int n)
+    {
+        a[0]=arr[0];
+        for(int i=1;i<n;++i)
+        {
+            a[i]=a[i-1]^arr[i];
+        }
+        for(int i:a)
+            cout<<i;
+    }
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) 
     {
+        int n=arr.size();
+        vector<int>r(n);
+        Prefix_Xor(r,arr,n);
         vector<int> result;
-        int t;
+        int i,j;
         for(auto a:queries)
         {
-            t=0;
-            for(int i=a[0];i<=a[1];i++)
-                t=t^arr[i];
-            result.push_back(t);
+            i=a[0];
+            j=a[1];
+            if(i==0)
+                result.push_back(r[j]);
+            else
+                result.push_back(r[j]^r[i-1]);
         }
         return result;
     }
