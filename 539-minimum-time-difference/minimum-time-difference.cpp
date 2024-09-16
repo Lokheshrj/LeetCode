82890c4fdf088cad36fe686233396f6da60d900e
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-09-16 18:09:59 +0530

    Time: 11 ms (69.95%) | Memory: 17.4 MB (88.24%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.cpp
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -9,14 +9,15 @@ public:
 
         sort(minutes.begin(),minutes.end());
 
-        int minimum=INT_MAX,n=minutes.size();
+        int minimum=INT_MAX;
+        short int n=minutes.size();
 
         for(int i=0;i<n-1;i++)
             if((minutes[i+1]-minutes[i])<minimum)
                 minimum=minutes[i+1]-minutes[i];
 
-        if(((24*60)-minutes[n-1]+minutes[0])<minimum)
-            return (24*60)-minutes[n-1]+minutes[0];
-        return minimum; 
+        int c=(24*60)-minutes[n-1]+minutes[0];
+        
+        return c<minimum?c:minimum;
     }
 };
