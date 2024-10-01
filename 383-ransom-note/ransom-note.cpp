3aff1fc7930acd6f93a47b14bce0741bf13b900b
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-10-01 19:16:26 +0530

    Time: 25 ms (12.90%) | Memory: 11.7 MB (14.49%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) 
+    {
+        
+        map<char,int>mp;
+        int c=0;
+        for(char ch:ransomNote)
+        {
+            ++mp[ch];
+        }
+        for(char ch:magazine)
+        {
+            if(mp[ch]>0)
+                --mp[ch];
+        }
+        for(auto a:mp)
+        {
+            if(a.second>0)
+                return false;
+        }
+        return true;
+    }
+};
