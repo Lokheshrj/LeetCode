328462485a774e98ae6d90f865b1a4d5ab5e49d2
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-10-01 13:23:08 +0530

    Time: 58 ms (37.22%) | Memory: 38.7 MB (34.04%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -4,20 +4,14 @@ public:
     {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        int max=nums[n-1]<n?n:nums[n-1];
-        map<int,int>mp;
-        int temp=0;
-        vector<int>ans(max,0);
-        vector<int>nu;
+        vector<int>ans(n,0);
+        vector<int>res;
         for(int i=0;i<n;i++)
-        {
             ans[nums[i]-1]=1;
-        }
+
         for(int i=0;i<n;i++)
-        {
             if(ans[i]==0)
-                nu.push_back(i+1);
-        }
-        return nu;
+                res.push_back(i+1);
+        return res;
     }
 };
