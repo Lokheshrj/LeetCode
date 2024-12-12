65e04c874dd2bb0bb743f441b6c3a051789aa3f3
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-12-12 19:04:17 +0530

    Time: 3 ms (54.53%) | Memory: 13 MB (78.35%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
@@ -2,15 +2,22 @@ class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) 
     {
+        priority_queue<int> q;
         long long s=0;
+        int x;
+        for(int x: gifts)
+        {
+            q.push(x);
+            s+=x;
+        }
         while(k)
         {
-            make_heap(gifts.begin(),gifts.end());
-            gifts[0]=floor(sqrt(gifts[0]));
-            --k;  
+            x=q.top();
+            s-=x-sqrt(x);
+            q.pop();
+            q.push(sqrt(x));
+            --k;
         }
-        for(int x:gifts)
-            s+=x;
         return s;
-        }
+    }
     };
