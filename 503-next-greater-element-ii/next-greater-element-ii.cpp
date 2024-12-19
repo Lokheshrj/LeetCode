9e1d857a15a97e2d2f1becd37f666deb011047b3
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2024-12-20 00:08:38 +0530

    Time: 104 ms (9.92%) | Memory: 29.4 MB (7.06%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    void traversal(stack<int>&s,vector<int>&nums ,int i,vector<int>&ans)
+    {
+        while(!s.empty() && nums[i] >= nums[s.top()])
+            {
+                s.pop();
+            }
+            if(!s.empty()&&nums[s.top()]>nums[i])
+               ans[i]=nums[s.top()]; 
+            else
+                ans[i]=-1;
+            s.push(i);
+        return;
+    }
+    vector<int> nextGreaterElements(vector<int>& nums) 
+    {
+        stack<int>s;
+        int n=nums.size();
+        vector<int>ans(n);
+        int i=n-1;
+        while(i>=0)
+        {
+            traversal(s,nums,i,ans);
+            i--;
+        }
+        i=n-1;
+        while(i>=0)
+        {
+            traversal(s,nums,i,ans);
+            i--;
+        }
+        // for(auto x:mp)
+        // {
+        //     cout<<x.first<<x.second<<"\n";
+        // }
+        for(int x: ans)
+        {
+            cout<<x;
+        }
+        
+            
+        return ans;
+    }
+};
