759bf7ede8ce7c1a6acb31c3f5ed5dd9399c8b43
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2025-01-09 09:52:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (85.23%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) 
     {
-        int size=words.size(),total=0;
-        for(string word:words)
+        int total=0;
+        for(int j=0;j<words.size();j++)
         {
             for(int i=0;i<pref.size();i++)
             {
-                if(word[i]!=pref[i])
+                if(words[j][i]!=pref[i])
                 {
                     ++total;
                     break;
-
                 }
             }
         }
-        return size-total;
+        return words.size()-total;
     }
 };
