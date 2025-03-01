c703e2479374067080b122292241979ab9fb6e8d
Lokhesh <94900012+Lokheshrj@users.noreply.github.com>
2025-03-01 16:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (52.78%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -22,19 +22,10 @@ public:
 
         for(int i=1;i<r;i++)
         {
-            if(matrix[i][0]==0)
+            for(int j=1;j<c;j++)
             {
-                for(int j=1;j<c;j++)
+                if(matrix[0][j]==0||matrix[i][0]==0)
                     matrix[i][j]=0;
-            }
-        }
-
-        for(int i=1;i<c;i++)
-        {
-            if(matrix[0][i]==0)
-            {
-                for(int j=1;j<r;j++)
-                    matrix[j][i]=0;
             }
         }
 
